49eac95c6e2fef71edbcdc758768b449905608d6
oscarsaavedrax <103982163+oscarsaavedrax@users.noreply.github.com>
2023-04-19 13:20:54 -0700

    Change server, add procfile

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ import express from "express";
 import path from "path";
 const PORT = process.env.PORT || 8080;
 const app = express();
-app.use(express.static("/"));
-app.use(express.static(path.join("/", "build")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join("build", "index.html"));
+  });
+}
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "build")));
 app.get("/ping", function (req, res) {
   return res.send("pong");
 });
 app.get("/*", function (req, res) {
-  res.sendFile(path.join("/", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 app.listen(PORT);
